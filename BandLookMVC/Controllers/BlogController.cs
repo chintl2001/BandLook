f1e4c3b53c7c7cc013c41780efd8a897c2c30dba
chintl2001 <nguyenlinhchi@tqp.com>
2025-03-26 23:45:49 +0700

    feat: blog

---
+++ BandLookMVC/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BandLookMVC.Controllers;
+
+public class BlogController : Controller
+{
+
+    public async Task<IActionResult> List()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Detail()
+    {
+        return View();
+    }
+}
